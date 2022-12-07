92e97511c2035e85b26dfcf0a12703790bc29855
FedericoBuccella <fedec.fb@gmail.com>
2022-12-07 14:41:06 -0300

    Arreglo puerto

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 import path from "path"
 import { completarDatos, sendVideo } from "./function.js"
 import * as url from 'url';
+import { PORT } from "./config.js"; 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const port = 8080
 
 app.use(express.static(path.join(__dirname, "/views")))
 app.use(express.json())
@@ -13,4 +13,4 @@ app.use(express.urlencoded({ extended: true }))
 app.get("/entrevista", completarDatos)
 app.get("/grabarVideo", sendVideo)
 
-app.listen(port, () => {console.log(`Servidor estableciendo conexión con el puerto: ${port}`)})
+app.listen(PORT, () => {console.log('Servidor estableciendo conexión con el puerto')})
